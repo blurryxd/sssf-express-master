1042f9863988f78532f2f26e1818fddc823e2840
Juho <juho.huhtanen@windowslive.com>
2020-03-27 18:27:31 +0200

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Tasks
+
+Respond to GET /cats with gender, age and weight query parameters with JSON containing the filtered list of cats. E.g. GET /cats?gender=male&weight=10&age=11 will return the male cats that have weight over 10kg and are older than 11 years.
+
+Didn't get this to work. This is the way I would've approached it adding more queries wtih '/:gender:weight:age'nod
+
+
+Video of jelastic deployment:
+
+https://drive.google.com/open?id=13f0fxzEZC6kTFWQJZ2F4M9-CAl-DBIsL
--- routes/catsRoute.js
+++ routes/catsRoute.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
 //>Respond to GET /cats with gender, age and weight query parameters with JSON containing the filtered list of cats.
 // E.g. GET /cats?gender=male&weight=10&age=11 will return the male cats
 // that have weight over 10kg and are older than 11 years.
-// Didn't get this to work. This is the way I would've approached it adding more queries wtih '/:gender:weight:age'
+// Didn't get this to work. This is the way I would've approached it adding more queries wtih '/:gender:weight:age'nod
 router.get('/:gender', async (req, res) => {
     res.send(await catModel.find().where({gender: req.params.gender}))
 });
